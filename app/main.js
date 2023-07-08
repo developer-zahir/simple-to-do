b7cc37c884fabb5bf8e89267613f4f9834f303f8
Developer zahir <82234171+developer-zahir@users.noreply.github.com>
2023-07-09 02:44:38 +0600

    Update main.js

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const reload_button = document.querySelector(".reload");
 const error_message = document.querySelector(".error_message");
 
 // store data
-let foods = ["Regular class korte hobe. ", "Prochur practice korte hobe. ", " Protita class er note korete hobe", "Ekjon valo maner, developer hote hobe. ", "Ekjon valo mner manush hote hobe"];
+let foods = [];
 
 // food processing from array
 const food_processing = () => {
